3f6b361c2523b1d6988f0721f6a88b72836531aa
josueramirez1 <josue67ramirez@gmail.com>
2026-01-24 14:16:54 -0800

    chaing import routes

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,6 @@
-import Features from "@/components/features";
-import Footer from "@/components/footer";
-import Hero from "@/components/hero";
+import Features from "../components/features";
+import Footer from "../components/footer";
+import Hero from "../components/hero";
 
 const LandingPage = () => {
   return (
